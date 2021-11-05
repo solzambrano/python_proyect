39084d259c5175754b9b71c82faf4233e02155e4
Soledad Zambrano <soe_28_16@hotmail.com>
2021-11-05 20:34:41 -0300

    clase 1 python

---
+++ main.py
@@ -0,0 +1,19 @@
+y=123
+y="hola mundo"
+#se dejan  espacios despues de la funcion
+"""sumar dos numeros a y b"""
+def sumar(y,b):
+    return y+b
+
+def restar(a,b):
+    return a-b
+
+lista=[1,2,4,5,3,6,7]
+print(3 in lista)#da true
+print (10 in lista)
+
+count=0
+while count < 5:
+    print(count)
+    count=int(input("ingrese un numero: "))
+print("fin del programa")
